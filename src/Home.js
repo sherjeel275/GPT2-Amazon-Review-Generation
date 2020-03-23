2259adf85ac7c4287551b97e87b701f011b72b00
reinvald <reinvald@seas.upenn.edu>
2020-03-23 12:49:04 -0400

    updating play page

--- src/Home.js
+++ src/Home.js
@@ -1,44 +1,63 @@
-import React from 'react'
+import React from "react";
 
 class Home extends React.Component {
-
   render() {
     return (
       <React.Fragment>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="/">Home</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <a class="navbar-brand" href="/">
+            Home
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-
-            <li class="nav-item">
-                <a class="nav-link" href="About">About</a>
-            </li>
-             <li class="nav-item">
-                  <a class="nav-link" href="Play">Play</a>
+              <li class="nav-item">
+                <a class="nav-link" href="About">
+                  About
+                </a>
               </li>
               <li class="nav-item">
-                  <a class="nav-link" href="Analytics">Analytics</a>
+                <a class="nav-link" href="Play">
+                  Play
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Analytics">
+                  Analytics
+                </a>
               </li>
 
               <li class="nav-item">
-                  <a class="nav-link" href="Team">Team</a>
+                <a class="nav-link" href="Team">
+                  Team
+                </a>
               </li>
-              
             </ul>
           </div>
         </nav>
         <div class="jumbotron text-center">
-          <h1><b> <font face = "Verdana" size = "24" color ="#255a7b">Welcome to GuessAI!</font></b></h1>
+          <h1>
+            <b>
+              {" "}
+              <font face="Verdana" size="24" color="#255a7b">
+                Welcome to AMiAI!
+              </font>
+            </b>
+          </h1>
         </div>
       </React.Fragment>
     );
-
   }
 }
 
-
-
-export default Home
+export default Home;
--- src/Play.js
+++ src/Play.js
@@ -1,60 +1,99 @@
-import React from 'react';
+import React from "react";
 import Home from "./Home";
-import ReactDOM from 'react-dom';
-import './Play.css'
-
+import ReactDOM from "react-dom";
+import "./Play.css";
 
 class Play extends React.Component {
-	render() {
-    	return (
-	      <React.Fragment>
-	        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	          <a class="navbar-brand" href="/">Home</a>
-	          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	            <span class="navbar-toggler-icon"></span>
-	          </button>
-	          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-	            <ul class="navbar-nav mr-auto">
+  state = {
+    count: 1
+  };
 
-	            <li class="nav-item">
-                	<a class="nav-link" href="About">About</a>
-            	</li>
-            	 <li class="nav-item">
-                	<a class="nav-link" href="Play">Play</a>
-            	</li>
-            	<li class="nav-item">
-                	<a class="nav-link" href="Analytics">Analytics</a>
-            	</li>
+  incrementRound = () => {
+    if (this.state.count == 15) {
+      // END GAME
+      this.setState({ count: 1 });
+    } else {
+      this.setState({ count: this.state.count + 1 });
+    }
+  };
 
-            	<li class="nav-item">
-                	<a class="nav-link" href="Team">Team</a>
-            	</li>
-	              
-	            </ul>
-	          </div>
-	        </nav>
-	        <div class="jumbotron text-center">
-		      <h1><b> <font face = "Verdana" size = "24" color ="#255a7b">Try and guess which of the following passages are AI generated!</font></b></h1>
-		        <section class="container">
-	  				<div class="left-half">
-	    				<article>
-	     					<h1>AI Generated</h1>
-	      						<p>Here is the AI generated text. We should find a way to get this text box to release the passage line by line. </p>
-	    				</article>
-	  				</div>
-	  				<div class="right-half">
-	    				<article>
-	      					<h1>Human Generated</h1>
-	      						<p>Here is the human generated text. Also we need to probably randomize which side it shows up on.</p>
-	    				</article>
-	 		 		</div>
-				</section>
-	        </div>
-	      </React.Fragment>
-    	);
+  render() {
+    return (
+      <React.Fragment>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="/">
+            Home
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="About">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Play">
+                  Play
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Analytics">
+                  Analytics
+                </a>
+              </li>
 
-  	}
-
+              <li class="nav-item">
+                <a class="nav-link" href="Team">
+                  Team
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div class="jumbotron text-center">
+          <h1>
+            <b>
+              {" "}
+              <font face="Verdana" size="24" color="#255a7b">
+                Guess if the following product review was AI generated!
+              </font>
+            </b>
+          </h1>
+          <span>Current round: {this.state.count}</span>
+          <section class="container">
+            <div class="review-box">
+              <article>
+                <p>Review text to be inserted here...</p>
+              </article>
+            </div>
+          </section>
+        </div>
+        <button
+          onClick={this.incrementRound}
+          className="btn btn-secondary btn-sm"
+        >
+          Real
+        </button>
+        <button
+          onClick={this.incrementRound}
+          className="btn btn-secondary btn-sm"
+        >
+          Fake
+        </button>
+      </React.Fragment>
+    );
+  }
 }
 
-export default Play
+export default Play;
