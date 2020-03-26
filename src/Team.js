29616cd4b28f93b58c6ad3510fcb08563be22a76
reinvald <reinvald@seas.upenn.edu>
2020-03-26 18:17:53 -0400

    started building meet the team page

--- src/Team.js
+++ src/Team.js
@@ -1,48 +1,115 @@
-import React from 'react';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
 import Home from "./Home";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
 class Team extends React.Component {
-	render() {
-    	return (
-	      <React.Fragment>
-	        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	          <a class="navbar-brand" href="/">Home</a>
-	          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	            <span class="navbar-toggler-icon"></span>
-	          </button>
-	          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-	            <ul class="navbar-nav mr-auto">
-
-	            <li class="nav-item">
-                	<a class="nav-link" href="About">About</a>
-            	</li>
-            	 <li class="nav-item">
-                	<a class="nav-link" href="Play">Play</a>
-            	</li>
-            	<li class="nav-item">
-                	<a class="nav-link" href="Analytics">Analytics</a>
-            	</li>
-
-            	<li class="nav-item">
-                	<a class="nav-link" href="Leaderboard">Leaderboard</a>
-            	</li>
-	             
-	             <li class="nav-item">
-                	<a class="nav-link" href="Team">Team</a>
-            	</li>
-
-	            </ul>
-	          </div>
-	        </nav>
-	        <div class="jumbotron text-center">
-	          <h1><b> <font face = "Verdana" size = "24" color ="#255a7b">Meet the team</font></b></h1>
-	        </div>
-	      </React.Fragment>
-    	);
-
-  	}
-
+  render() {
+    return (
+      <React.Fragment>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="/">
+            Home
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="About">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Play">
+                  Play
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Analytics">
+                  Analytics
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Team">
+                  Team
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div class="jumbotron text-center">
+          <h1>
+            <b>
+              {" "}
+              <font face="Verdana" size="24" color="#255a7b">
+                Meet the team!
+              </font>
+            </b>
+          </h1>
+          <br></br>
+          <br></br>
+          <CardDeck>
+            <Card border="primary" style={{ width: "18rem" }}>
+              <Card.Header>Sherjeel Arif</Card.Header>
+              <Card.Img variant="top" src="" />
+              <Card.Body>
+                <Card.Text>
+                  Hi, my name is Sherjeel and I'm a senior at Penn studying
+                  Electrical Engineering with a concentration in Data Science.
+                  I'm interested in machine learning and deep learning. I hope
+                  to apply these disciples to better understand our world!
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card border="primary" style={{ width: "18rem" }}>
+              <Card.Header>Srinath Makesh</Card.Header>
+              <Card.Img variant="top" src="" />
+              <Card.Body>
+                <Card.Text>...</Card.Text>
+              </Card.Body>
+            </Card>
+            <Card border="primary" style={{ width: "18rem" }}>
+              <Card.Header>Aavo Reinvald</Card.Header>
+              <Card.Img variant="top" src="" />
+              <Card.Body>
+                <Card.Text>
+                  Hello! My name is Aavo and I am a senior studing CS at Penn.
+                  My interests currently lie in algorithm design, game theory,
+                  software optimization, and humanitarian software development.
+                  I'll be a software developer in Chicago after I complete my
+                  degree.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <Card border="primary" style={{ width: "18rem" }}>
+              <Card.Header>Andrew Si</Card.Header>
+              <Card.Img variant="top" src="" />
+              <Card.Body>
+                <Card.Text>
+                  My name is Andrew and I'm a senior at Penn majoring in
+                  computer science. Some of my interests in CS include natural
+                  language processing, computer vision, and computer graphics.
+                  I'll be working in software engineering in the San Francisco
+                  Bay Area after graduating.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </CardDeck>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-export default Team
+export default Team;
