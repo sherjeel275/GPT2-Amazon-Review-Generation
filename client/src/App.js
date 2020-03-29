19d2efbc049b9cc0b79d4ac22708054224c9eaf3
reinvald <reinvald@seas.upenn.edu>
2020-03-28 20:56:43 -0400

    build node proxy and connected to atlas

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
-import './App.css'
-import Login from './Login';
-import Signup from './Signup';
-import Logout from './Logout'
-import Home from './Home';
-import About from './About';
-import Play from './Play';
-import Analytics from './Analytics';
-import Team from './Team';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Login from "./Login";
+import Signup from "./Signup";
+import Logout from "./Logout";
+import Home from "./Home";
+import About from "./About";
+import Play from "./Play";
+import Analytics from "./Analytics";
+import Team from "./Team";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
   render() {
@@ -28,77 +22,102 @@ class App extends React.Component {
               path="/"
               render={() => (
                 //this.state.isLoggedIn === true
-                  //? <Home />
-                  //: <Redirect to="/login" />
-                  <Home />
+                //? <Home />
+                //: <Redirect to="/login" />
+                <Home />
               )}
             />
             <Route
               path="/login"
               render={() => (
-              //  this.state.isLoggedIn === true
-               //   ? <Home />
-               //   : <Login />
-               <Login />
+                //  this.state.isLoggedIn === true
+                //   ? <Home />
+                //   : <Login />
+                <Login />
               )}
             />
-            <Route
-              path="/signup"
-              render={() => (
-                <Signup />
-              )}
-            />
+            <Route path="/signup" render={() => <Signup />} />
             <Route
               path="/About"
               render={() => (
                 //this.state.isLoggedIn === true
-                 // ? <About />
-                 // : <Login />
-                 <About />
+                // ? <About />
+                // : <Login />
+                <About />
               )}
             />
             <Route
               path="/Play"
               render={() => (
                 //this.state.isLoggedIn === true
-                 // ? <Play />
-                 // : <Login />
-                 <Play />
+                // ? <Play />
+                // : <Login />
+                <Play />
               )}
             />
             <Route
               path="/Analytics"
               render={() => (
                 //this.state.isLoggedIn === true
-                 // ? <Analytics />
-                 // : <Login />
-                 <Analytics />
+                // ? <Analytics />
+                // : <Login />
+                <Analytics />
               )}
             />
             <Route
               path="/Team"
               render={() => (
                 //this.state.isLoggedIn === true
-                 // ? <Team />
-                 // : <Login />
-                 <Team />
+                // ? <Team />
+                // : <Login />
+                <Team />
               )}
             />
-            <Route
-              path='/logout'
-              render={() => (
-                <Logout />
-               )}
-            />
+            <Route path="/logout" render={() => <Logout />} />
           </Switch>
         </Router>
       </div>
-
-      );
+    );
   }
-
 }
 
+export default App;
 
+/*
+// /client/src/App.js
+
+import React, { useState, useEffect } from "react";
+
+// SERVICES
+import productService from "./services/productService";
+
+function App() {
+  const [products, setproducts] = useState(null);
+
+  useEffect(() => {
+    if (!products) {
+      getProducts();
+    }
+  });
+
+  const getProducts = async () => {
+    let res = await productService.getAll();
+    console.log("1");
+    console.log(res);
+    setproducts(res);
+  };
+
+  const renderProduct = product => {
+    return (
+      <li key={product._id} className="list__item product">
+        <h3 className="product__name">{product.name}</h3>
+        <p className="product__description">{product.description}</p>
+      </li>
+    );
+  };
+
+  return <div className="App">hello</div>;
+}
 
 export default App;
+*/
--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 //  index.js
 
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const { MongoClient } = require("mongodb");
 
 const app = express();
-
-//mongoose.Promise = global.Promise;
-//mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
-
-/*
 const MongoClient = require("mongodb").MongoClient;
 const uri =
   "mongodb+srv://reinvald:mepP8NhdN93IY6Vm@cluster0-xf5wa.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+app.use(bodyParser.json());
 
-*/
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 async function main() {
-  /**
-   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
-  const uri =
-    "mongodb+srv://reinvald:mepP8NhdN93IY6Vm@cluster0-xf5wa.mongodb.net/test?retryWrites=true&w=majority";
-
-  const client = new MongoClient(uri);
-
   try {
     // Connect to the MongoDB cluster
     await client.connect();
@@ -59,9 +46,20 @@ async function listDatabases(client) {
 
 main().catch(console.error);
 
-app.use(bodyParser.json());
+app.get("/test", function(req, res) {
+  console.log("TESTING");
+  res.send("HELLO!");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
+
+/*
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
